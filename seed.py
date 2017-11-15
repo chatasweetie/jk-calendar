dc7a6158163eb1d3098edc1cef9ae8c489cf7ff6
KaraLyn DeWalt <karalyn.dewalt@gmail.com>
2017-11-14 16:56:09 -0800

    adds events and invite functions

--- seed.py
+++ seed.py
@@ -1,5 +1,6 @@
 import model
 
+
 USERS = ["sally@gmail.com",
            "bill@gmail.com",
            "alice@aol.com",
@@ -7,6 +8,46 @@ USERS = ["sally@gmail.com",
            ]
 
 PERMISSIONS = ["view", "edit", "owner",]
+
+STATUSES = ["awaiting response", "accepted", "declined", "edited"]
+
+EVENTS = [{"title": "dinner",
+           "start": model.datetime(2017, 11, 14, 15, 30),
+           "end":  model.datetime(2017, 11, 14, 16, 30),
+            },
+            {"title": "Birthday!",
+             "start": model.datetime(2017, 11, 20, 15, 30),
+             "end":  model.datetime(2017, 11, 20, 16, 30),
+            },
+            {"title": "Shopping!",
+             "start": model.datetime(2017, 11, 24, 15, 30),
+             "end":  model.datetime(2017, 11, 24, 16, 30),
+            },
+            {"title": "Buy Suit!",
+             "start": model.datetime(2017, 11, 30, 15, 30),
+             "end":  model.datetime(2017, 11, 30, 16, 30),
+            },
+            {"title": "Walk River!",
+             "start": model.datetime(2017, 11, 16, 15, 30),
+             "end":  model.datetime(2017, 11, 16, 16, 30),
+            },
+            {"title": "Jesus Birthday!",
+             "start": model.datetime(2017, 11, 25, 15, 30),
+             "end":  model.datetime(2017, 11, 25, 16, 30),
+            },
+            {"title": "Code!",
+             "start": model.datetime(2018, 11, 20, 15, 30),
+             "end":  model.datetime(2017, 11, 20, 16, 30),
+            },
+            {"title": "Holiday Party!",
+             "start": model.datetime(2017, 11, 27, 15, 30),
+             "end":  model.datetime(2017, 11, 27, 16, 30),
+            },
+            {"title": "Icecream!",
+             "start": model.datetime(2017, 11, 23, 15, 30),
+             "end":  model.datetime(2017, 11, 23, 16, 30),
+            },
+            ]
 
 
 def create_permissions(p_list):
@@ -58,12 +99,71 @@ def create_cal():
     model.db.session.commit()
 
 
+def create_status(s_list):
+    """Creates and commits statuses
+
+    """
+
+    for status in s_list:
+        new_stat = model.Status(status=status)
+
+        model.db.session.add(new_stat)
+
+    model.db.session.commit()
+
+
+def make_event(event, cal_id):
+
+    return model.Event(cal_id=cal_id,
+                        title=event["title"],
+                        start=event["start"],
+                        end=event["end"])
+
+def create_events(e_list):
+    """Creates and commits events
+
+    """
+
+    cals = model.Calendar.query.all()
+    count = 0
+
+    for cal in cals:
+
+        event_1 = make_event(e_list[count], cal.cal_id)
+        event_2 = make_event(e_list[count + 1], cal.cal_id)
+        count += 2
+
+        model.db.session.add_all([event_1, event_2])
+
+    model.db.session.commit()
+
+
+def create_invites():
+
+    invite = model.Event_Invite(event_id=1,
+                                status_id=1,
+                                cal_id=4)
+    model.db.session.add(invite)
+    model.db.session.commit()
+
+
 if __name__ == '__main__':
 
     from server import app
+    import os
+
+    print "Dropping DB"
+    print "***********"
+    os.system("dropdb jkcalendar")
+    print "Creating DB"
+    print "***********"
+    os.system("createdb jkcalendar")
 
     model.connect_to_db(app)
 
     create_permissions(PERMISSIONS)
     create_users(USERS)
     create_cal()
+    create_status(STATUSES)
+    create_events(EVENTS)
+    create_invites()
